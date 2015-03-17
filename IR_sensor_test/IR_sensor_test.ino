972f5bc658c7cb38fc45cf55873c9d95e950c071
Salavat Kh <x.salik@ya.ru>
2015-03-17 16:44:36 +0500

    Revert "new"
    This reverts commit 4d3f19eba0d53be7730e46c6f2e73338d045fc3d.

--- IR_sensor_test/IR_sensor_test.ino
+++ IR_sensor_test/IR_sensor_test.ino
@@ -1,13 +1,11 @@
+#include <Ultrasonic.h>
 #include <RunningMedian.h>
 
-RunningMedian Median50 = RunningMedian(30); // Медианный фильтр на исходные данные - вольты
-RunningMedian Median50_2 = RunningMedian(30); // Медианный фильтр на сантиметры
+RunningMedian Median50 = RunningMedian(50);
 
-int sensorpin = 0;  // Номер аналогового входа в ардуино
-float val = 0;      // Переменная для чтения исх. данных с сенсора 
-float distance;     // Переменная для расчета дистанции в см 
-float distance_filtered; // Переменная для расчета дистанции в см с фильтром 1
-float distance_filtered2;// Переменная для расчета дистанции в см с фильтром 2
+int sensorpin = 0;  // analog pin used to connect the sharp sensor
+float val = 0; 
+float distance;
 
 const int numberOfEntries = 20;
 
@@ -16,21 +14,18 @@ void setup() {
 }
 
 void loop() {
-   
-   for (int i=0; i < numberOfEntries; i++)
-    {
-      val = analogRead(sensorpin); // Добавляем в фильтр исходные данные - вольты
-      Median50.add(val);
-      delay(15);
-    } 
-    
-  distance = 13*pow(val*0.0048828125, -1) ;
-  Median50_2.add(distance); // Добавляем в фильтр сантиметры
   
-  distance_filtered  = 13*pow(Median50.getAverage()*0.0048828125, -1);
-  distance_filtered2 = Median50_2.getAverage();
+  for (int i=0; i < numberOfEntries; i++)
+  {
+    val = analogRead(sensorpin);
+    Median50.add(val);
+    delay(5);
+  } 
+  
+  distance = 2400/(val - 20);
+  
+  float a50 = 2400/(Median50.getAverage()-20);
    
   Serial.print(distance); Serial.print("-");
-  Serial.print(distance_filtered);Serial.print("-");
-  Serial.println(distance_filtered2);
+  Serial.println(a50);
 }
